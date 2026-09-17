da6f95a79ca6807d1fbdd9dd15e2f41821e420a5
harxhtttt <jhah0648@gmail.com>
2026-09-18 04:00:22 +0530

    Apply suggestion from @git-genie02[bot]
    Co-authored-by: git-genie02[bot] <330288276+git-genie02[bot]@users.noreply.github.com>

--- fixtest.js
+++ fixtest.js
@@ -1,9 +1,8 @@
 function hasCommonElement(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        return true;
-      }
+  const set = new Set(arr1);
+  for (let i = 0; i < arr2.length; i++) {
+    if (set.has(arr2[i])) {
+      return true;
     }
   }
   return false;
